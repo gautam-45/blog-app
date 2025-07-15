a2bf4727a5f72c9d8ef11f4cd5b485a5c1859b82
gautam-45 <gautampawar4510@gmail.com>
2025-07-15 23:36:20 +0530

    Remove secret file from version control

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const supabaseUrl = 'https://cecpovpnyozgfztppdhk.supabase.co';
-  static const supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlY3BvdnBueW96Z2Z6dHBwZGhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1MjI2MjksImV4cCI6MjA2NjA5ODYyOX0.OFrFkWmAOD9xR57izWYc8C41e8jGa6PTSySuHrSBm7U';
-}
